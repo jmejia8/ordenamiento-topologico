5b4a1703056eba24472a00c7a02efd8d554b08b2
jesus <jesusmejded@gmail.com>
2016-08-30 23:33:50 -0500

    Ordenamiento topológico completo

--- main.c
+++ main.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Número de nodos
+#define NODES	7
+
+const char names[] = {'A', 'B', 'C', 'D', 'E',
+					'F', 'G', 'H', 'I', 'J',
+					'K', 'L', 'M', 'N', 'O',
+					'P', 'Q', 'R', 'S', 'T',
+					'U', 'V', 'W', 'X', 'Y', 'Z'};
+
 // queue structure
 typedef struct _node
 {
 	char id;
+	int index;
 	struct _node* back;
 } Queue;
+
+char out_names[NODES];
+char out_names_counter = 0;
 
 //
 // General fuction for the queue
 // Enqueue 
 // Dequeue
 //
-int enqueue(Queue** front, Queue** end, char data){
+int enqueue(Queue** front, Queue** end, char data, int index){
 	Queue* new_node = (Queue *) malloc(sizeof(Queue));
 
 	// if somethig is wrong return false
@@ -24,12 +37,13 @@ int enqueue(Queue** front, Queue** end, char data){
 	}
 
 	new_node->id  = data;
+	new_node->index  = index;
 
 	if (*end != NULL){
 		Queue* aux = *end;
 		aux->back = new_node; 
 	}
-	
+
 	*end = new_node; 
 	
 	// if there's no elements in queue
@@ -43,10 +57,7 @@ int dequeue(Queue** front, Queue** end){
 
 
 	if (*front == NULL)
-	{
-		printf(">>>>Falso\n");
-		return 0;
-	}
+		return -1;
 
 
 	Queue* aux = *front;
@@ -62,39 +73,123 @@ int dequeue(Queue** front, Queue** end){
 	}
 
 	
-	printf("%c, ", aux->id);
+	out_names[out_names_counter] = aux->id;
+	out_names_counter++;
+
+	int tmp = aux->index;
 
 	free(aux);
 
-	return 1;	
+	return tmp;	
 }
 
+int count_presreq(int matrix[NODES][NODES], int *pres, int nodes){
+	// Cuenta los prerrequisitos
+	// de cada nodo
 
+	int i, j, flag = 0;
+	for (i = 0; i < nodes; ++i){
+		// Si ya revisé esté nodo, no lo cuento
+		if (pres[i] == -1) continue;
+
+		pres[i] = 0;
+
+		for ( j = 0; j < nodes; ++j){
+			pres[i] += matrix[j][i];
+		}
+
+		// Si hay almenos un nodo con prerrequisitos
+		if (pres[i] > 0)
+			flag = 1;
+	}
+
+	return flag;
+}
+
+void topo_order(int matrix[NODES][NODES], int* pres, int nodes, Queue** _front, Queue** _end){
+
+	Queue* front = *_front;
+	Queue* end = *_end;
+	int i, j, flag, flag2 = 0;
+
+	// Regresa 0 si no hay prerequisitos
+	flag = count_presreq(matrix, pres, NODES);
+
+	for (i = 0; i < NODES; ++i){
+		if (pres[i] == 0){
+
+			// Se eliminan las conecciones del padre
+			for (j = 0; j < nodes; ++j) matrix[i][j] = 0; 
+
+			// se agrega a la cola
+			enqueue(&front, &end, names[i], i);
+			continue;
+		}
+
+		flag2++;
+
+	}
+
+	// cuenta los nodos con prerrequisitos
+	if (flag2 >= NODES) return;
+
+
+	int index = dequeue(&front, &end);
+
+	do {
+
+		// Si no hay elementos en la cola
+		// y ya no hay elementos a con prerrequisitos 
+		// igual a cero, entonces se termina
+		if (index == -1 && flag == 0) return;
+
+
+		// El -1 describe que ese elemento ya se imprimió 
+		// en pantalla
+		pres[index] = -1;
+
+		index = dequeue(&front, &end);
+
+	} while (index > 0);
+
+
+	topo_order(matrix, pres, NODES, &front, &end);
+}
 
 int main(int argc, char const *argv[])
 {
-	int nodes = 5;
-	int matrix[nodes][nodes];
 
+	// matriz de adyacencia
+	int matrix[NODES][NODES] = {
+		{0, 1, 1, 1, 0, 0, 1},
+		{0, 0, 1, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 1, 0},
+		{0, 0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0, 1},
+		{0, 0, 0, 0, 1, 0, 1},
+		{0, 0, 0, 0, 0, 0, 0}
+	};
+
+	// vector de pre-requisitos
+	int pres[NODES];
+	int i;
+
+	// apuntadores de la cola
 	Queue* front = NULL;
 	Queue* end = NULL;
 
-	printf("Inicio\n");
+	topo_order(matrix, pres, NODES, &front, &end);
 
-	enqueue(&front, &end, 'A');
-	enqueue(&front, &end, 'B');
-	enqueue(&front, &end, 'C');
+	if (out_names_counter >= NODES)
+	{
+		
+		for (i = 0; i < NODES; ++i) printf("%c, ", out_names[i]);
+		
+		printf("\n");
 
-	printf("------\n");
-
-	// Queue *perro = end;
-	// printf("%c\n", perro->id);
-
-
-	dequeue(&front, &end);
-	dequeue(&front, &end);
-	dequeue(&front, &end);
-	// dequeue(&front, &end);
+	}else{
+		printf("Grafo cíclico\n");
+	}
 
 
 	return 0;
